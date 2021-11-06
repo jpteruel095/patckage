ceae2047cf8d301dac79ad27c75c9749360e654f
John Patrick Teruel <jpteruel95@gmail.com>
2021-11-06 09:37:11 +0800

    feat: Create example app to demonstrate usage of the package

---
+++ .example/Pat-ckageSample/Services/Models/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  Pat-ckageSPMSample
+//
+//  Created by John Patrick Teruel on 11/4/21.
+//
+
+import Foundation
+import patckage
+
+struct User: ObjectCodable {
+    var name: String
+    var age: Int
+    var userID: String
+}
